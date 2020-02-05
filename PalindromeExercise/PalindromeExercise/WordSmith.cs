43c01fe644dc8a6d7539e7cd273ffe48fbb90c52
mvdoyle <mdoyle@truecoders.io>
2020-02-05 14:44:03 -0600

    Finished

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+namespace PalindromeExercise
+{
+    public class WordSmith
+    {
+        public WordSmith()
+        {
+        }
+
+        public bool IsAPalindrome(string word)
+        {
+            var reversed = "";
+
+            for (int i = word.Length - 1; i >= 0; i--)
+            {
+                reversed += word.ToLower()[i];
+            }
+
+            if(reversed == word.ToLower())
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+            //OR TRY THIS***
+            //return word.ToLower().SequenceEqual(word.ToLower().Reverse());
+        }
+    }
+}
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,14 +1,25 @@
 using System;
+using PalindromeExercise;
 using Xunit;
 
 namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("racecar", true)]
+        [InlineData("hello", false)]
+        [InlineData("Racecar", true)]
+        public void PalindromeTester(string word, bool expected)
         {
+            //Arrange
+            var wordSmith = new WordSmith();
 
+            //act
+            var actual = wordSmith.IsAPalindrome(word);
+
+            //assert
+            Assert.Equal(expected, actual);
         }
     }
 }
